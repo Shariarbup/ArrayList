18e3d26999b34b8b64c51ed4d8b02d44bbb30182
alimualrajisaikot <amrsaikot"gmail.com>
2018-05-09 11:15:49 +0600

    Arraylist

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,13 @@ public class Main {
 
 
     public static void main(String[] args) {
-        list_sec_b_prac list_sec_b_prac = new list_sec_b_prac();
-        list_sec_b_prac.getListItems();
 
-        linkedList_sec_b_prac linkedList_sec_b_prac = new linkedList_sec_b_prac();
-        linkedList_sec_b_prac.getLinkedList();
-
-        set_sec_b_prac set_sec_b_prac = new set_sec_b_prac();
-        set_sec_b_prac.getsetitem();
-        
-        list_sec_b_prac list_sec_b_prac1 = new list_sec_b_prac();
+        studentController studentController =new studentController();
+        for(student student : studentController.getStudentList()){
+            System.out.println(student.getStudentId());
+            System.out.println(student.getStudentName());
+            System.out.println(student.getSection());
+        }
 
 
     }
